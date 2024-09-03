ecd10de34d2acff3f0344568712dd686402573df
MafasAhmath <mafasathirms2@gmail.com>
2024-09-03 09:40:30 +0530

    witd side bar

--- SMS/src/app/app.component.ts
+++ SMS/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { SidebarComponent } from './sidebar/sidebar.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
