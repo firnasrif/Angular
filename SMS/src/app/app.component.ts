5dd25a54379e60555156bb49a3e6bb407d6aaadd
NaskathCader <naskathcader2000@gmail.com>
2024-09-03 14:00:57 +0530

    Resistration form

--- SMS/src/app/app.component.ts
+++ SMS/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegformComponent } from './forms/regform/regform.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SidebarComponent],
+  imports: [RouterOutlet,SidebarComponent,RegformComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
