b3ac30a96218f4774a73a6a31f1aa4a42e8ce61c
MafasAhmath <mafasathirms2@gmail.com>
2024-09-03 19:09:33 +0530

    side bar complete

--- SMS/src/app/sidebar/sidebar.component.ts
+++ SMS/src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink,RouterLinkActive,RouterOutlet],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  isCollapsed = false;
 
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
